eb8d07b3aaca3917c5b551e0f167c8bf74ed6806
Nathan Wiles <ntwiles@gmail.com>
2022-07-26 13:24:09 -0400

    Stub remove_from_cart command.

--- src/infrastructure/message_bus/handler.rs
+++ src/infrastructure/message_bus/handler.rs
@@ -4,6 +4,7 @@ use super::message::Message;
 
 #[async_trait]
 pub trait MessageHandler: Send + Sync {
+    // TODO: Find a solution that isn't "stringly typed".
     fn message_kind(&self) -> String;
     async fn handle(&self, message: &Message);
 }
